a5a8ec86b3232891fb0e2605577dfef7353aad2e
maxime le menaheze <maxime.le-menaheze@laposte.net>
2019-05-27 12:50:30 +0200

    4 step

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,22 @@
 class QuestionsController < ApplicationController
-    def ask
+  def ask
   end
 
   def answer
+    if params[:question] == "I am going to work"
+      @answer = "Great !"
+    elsif params[:question].include? '?'
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
   end
+
+  # app/controllers/restaurants_controller.rb
+
+  # def index
+  #   @category = params[:ask]
+  #   @restaurants = RESTAURANTS.select { |id, r| r[:category] == @category }
+  # end
+
 end
